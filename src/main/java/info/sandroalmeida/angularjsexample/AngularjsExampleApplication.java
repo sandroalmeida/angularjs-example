ba4c1f18528d80093ca84589f9fafe912e5b16fe
sandroalmeida <sandrodotbuild@gmail.com>
2019-04-07 08:59:42 -0700

    initial commit

---
+++ src/main/java/info/sandroalmeida/angularjsexample/AngularjsExampleApplication.java
@@ -0,0 +1,13 @@
+package info.sandroalmeida.angularjsexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularjsExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AngularjsExampleApplication.class, args);
+    }
+
+}
