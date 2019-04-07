611c091ad411b222b8a385c5d352b31e7cefb41e
sandroalmeida <sandrodotbuild@gmail.com>
2019-04-07 09:55:37 -0700

    createdUser API added

--- src/main/java/info/sandroalmeida/angularjsexample/UserController.java
+++ src/main/java/info/sandroalmeida/angularjsexample/UserController.java
@@ -5,10 +5,9 @@ import info.sandroalmeida.angularjsexample.repository.UserJpaRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -32,5 +31,12 @@ public class UserController {
         logger.debug("listAllusers called");
         List<UserDTO> users = (List) userJpaRepository.findAll();
         return new ResponseEntity<List<UserDTO>>(users, HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<UserDTO> createUser(@RequestBody final UserDTO user){
+        logger.debug("createUser called");
+        userJpaRepository.save(user);
+        return new ResponseEntity<UserDTO>(user, HttpStatus.CREATED);
     }
 }
